2f188a30ee849253ee34ecc43ee317aee66c8d0b
Дмиртий <@192.168.0.5>
2015-05-30 11:09:28 +0300

    Country

--- src/com/sourceit/javabasic/sungurov/L16/Country.java
+++ src/com/sourceit/javabasic/sungurov/L16/Country.java
@@ -4,10 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-import com.sun.imageio.plugins.common.InputStreamAdapter;
+//import com.sun.imageio.plugins.common.InputStreamAdapter;
 //import src.com.sourceit.java.basic.lesson16.lab.Country.txt;
 
 
@@ -19,18 +20,28 @@ public class Country {
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException {
-		 BufferedReader inputStream = null;
-		         PrintWriter outputStream = null;
-		 
+				BufferedReader inputStream = null;
+		        PrintWriter outputStream = null;
+		        
+		       SortedSet <String> mapUnCh = new TreeSet<String>() ;
+				
+		        
 		         try {
 		             inputStream = new BufferedReader(new FileReader("Country.txt"));
 		             outputStream = new PrintWriter(new FileWriter("CountrySort.txt"));
 		 
 		             String l;
 		             while ((l = inputStream.readLine()) != null) {
-		                 outputStream.println(l);
-		                 outputStream.println("---");
+		                 l=l.substring(6);
+		            	 mapUnCh.add(l);
+		            	 System.out.println();
+		            	 
 		             }
+
+		             for (String tr : mapUnCh ) {
+		             
+		             outputStream.println(tr);}
+		             
 		         } finally {
 		             if (inputStream != null) {
 		                 inputStream.close();
