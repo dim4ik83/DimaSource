321a5544313636c04c3dc676c5b077a2f9d31b98
Дмиртий <@192.168.0.5>
2015-06-01 23:10:01 +0300

    Country

--- src/com/sourceit/javabasic/sungurov/L16/Country.java
+++ src/com/sourceit/javabasic/sungurov/L16/Country.java
@@ -8,50 +8,49 @@ import java.io.PrintWriter;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-//import com.sun.imageio.plugins.common.InputStreamAdapter;
-//import src.com.sourceit.java.basic.lesson16.lab.Country.txt;
-
-
-
-
 public class Country {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException {
-				BufferedReader inputStream = null;
-		        PrintWriter outputStream = null;
-		        
-		       SortedSet <String> mapUnCh = new TreeSet<String>() ;
-				
-		        
-		         try {
-		             inputStream = new BufferedReader(new FileReader("Country.txt"));
-		             outputStream = new PrintWriter(new FileWriter("CountrySort.txt"));
-		 
-		             String l;
-		             while ((l = inputStream.readLine()) != null) {
-		                 l=l.substring(6);
-		            	 mapUnCh.add(l);
-		            	 System.out.println();
-		            	 
-		             }
+		BufferedReader inputStream = null;
+		PrintWriter outputStream = null;
 
-		             for (String tr : mapUnCh ) {
-		             
-		             outputStream.println(tr);}
-		             
-		         } finally {
-		             if (inputStream != null) {
-		                 inputStream.close();
-		             }
-		             if (outputStream != null) {
-		                 outputStream.close();
-		             }
-		         }
-        }
+		SortedSet<String> countrySet = new TreeSet<String>();
+		SortedSet<String> letterSet = new TreeSet<String>();
 
-	
+		try {
+			inputStream = new BufferedReader(new FileReader("Country.txt"));
+			outputStream = new PrintWriter(new FileWriter("CountrySort.txt"));
+
+			String l;
+			while ((l = inputStream.readLine()) != null) {
+				l = l.substring(4);
+				countrySet.add(l);
+			}
+
+			for (String str : countrySet) {
+				letterSet.add(str.charAt(0) + "");
+			}
+			
+			countrySet.addAll(letterSet);
+
+			for (String tr : countrySet) {
+				if (tr.length() > 2) {
+					outputStream.print("   ");
+				}
+				outputStream.println(tr);
+			}
+
+		} finally {
+			if (inputStream != null) {
+				inputStream.close();
+			}
+			if (outputStream != null) {
+				outputStream.close();
+			}
+		}
+	}
 
 }
