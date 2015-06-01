c05bc0107ef10db5a3e52512fbda3e695266d888
Дмиртий <@192.168.0.5>
2015-06-02 01:00:37 +0300

    Lorem

--- src/com/sourceit/javabasic/sungurov/HT15/Lorem.java
+++ src/com/sourceit/javabasic/sungurov/HT15/Lorem.java
@@ -1,170 +1,60 @@
 package com.sourceit.javabasic.sungurov.HT15;
+
 import com.sourceit.javabasic.sungurov.HT11.TextOp;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-
-
 
 public class Lorem {
 
-	
 	public static void main(String[] args) {
-		
-		String s="";
+
+		String s = "";
 		for (int i = 0; i < args.length; i++) {
 			s += args[i] + " ";
 		}
-		
+
 		TextOp ol = new TextOp(s);
-		
+
 		ol.getCharFrequency('f');
 		ol.getCharCount('f');
 		ol.getChars();
-		
 		ol.words();
-		
 		ol.getMostPopularCharCount();
-		
 		ol.getLeastPopularCharCount();
-		
-		
-		
-		try{
 
-			BufferedReader br =
+		PrintWriter outputStream = null;
 
-			new BufferedReader(new InputStreamReader(System.in, s)); 
+		try {
+			try {
+				outputStream = new PrintWriter(new FileWriter("LoremStat.txt"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-			PrintWriter pw = new PrintWriter(
+			outputStream.println("Statistics:");
+			outputStream.println("Frequency of the character " + 'f' + " is: "
+					+ ol.getCharFrequency('f') + "%");
+			outputStream.println("The character " + 'f' + " occurs in a text: "
+					+ ol.getCharCount('f') + " times");
+			outputStream
+					.println("Collection of unique characters and it's counts"
+							+ ol.getChars());
+			outputStream.println("list of words in a text" + ol.words());
+			outputStream.println("Count of a char which most popular: "
+					+ ol.getMostPopularCharCount());
+			outputStream.println("Count of a char which least popular: "
+					+ ol.getLeastPopularCharCount());
+			outputStream.println("------------------------");
 
-			new OutputStreamWriter(System.out, s), true); 
+			outputStream.println(s);
 
-			String s1 = "Это строка с русским текстом"; 
+		} finally {
+			if (outputStream != null) {
+				outputStream.close();
+			}
+		}
 
-			System.out.println("System.out puts: " + s1); 
-
-			pw.println("PrintWriter puts: " + s1) ; 
-
-			int с = 0;
-
-			pw.println("Посимвольный ввод:"); 
-
-			while((с = br.read()) != -1)
-
-//			pw.println((char) c); 
-
-			pw.println("Построчный ввод:"); 
-
-			do{
-
-			s = br.readLine(); 
-
-			pw.println(s); 
-
-			}while(!s.equals("q")); 
-
-			}catch(Exception e){
-
-			System.out.println(e); 
-
-			} 
-
-			} 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		
-//		BufferedReader br = null;
-//		try {
-//			br = new BufferedReader(new InputStreamReader(System.in,s));
-//			
-//			
-//			
-//			
-//			
-//		} catch (UnsupportedEncodingException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//					System.out.println("Enter String");
-//					String s1="";
-//					try {
-//						s1 = br.readLine();
-//					} catch (IOException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-//		
-//		System.out.println(s1);
-//		
-//		
-//		
-//		
-//	//	ol.
-//		
-//		
-//		
-//		
-//		BufferedReader inputStream = null;
-//		        PrintWriter outputStream = null;
-//		
-//		        try {
-////		            inputStream = new BufferedReader((s));
-//		            outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
-//		
-//		            String l;
-//		            while ((l = inputStream.readLine()) != null) {
-//		                outputStream.println(l);
-//		                outputStream.println("---");
-//		            }
-//		        } finally {
-//		            if (inputStream != null) {
-//		                inputStream.close();
-//		            }
-//		            if (outputStream != null) {
-//		                outputStream.close();
-//		            }
-//		        }
-//		
-//		
-//		
-//
-	
-	
+	}
 }
