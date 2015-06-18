430f12a158d4981c618a04bbcc2648d84fe7b2c4
Дмиртий <@192.168.0.5>
2015-06-18 23:36:49 +0300

    WGet

--- src/com/sourceit/javabasic/sungurov/lesson19/lab/WGet.java
+++ src/com/sourceit/javabasic/sungurov/lesson19/lab/WGet.java
@@ -1,49 +1,140 @@
 package com.sourceit.javabasic.sungurov.lesson19.lab;
 
-public class WGet {
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WGet  {
 
 	/**
 	 * @param args
 	 */
+	final static String commands [] = {"1) Save page", "2) Display title", "3) Find a word",
+		"4) List all url's on the page"};
+		
 	public static void main(String[] args) {
 		
-//		System.out.println("Hi");
-		
-		if (args.length==0) {
-			System.out.println("Usage: some web address.");
-			System.out.println("           or -i");
-			System.out.println("Example: http//google.com");
-			return;
-			
-		}
-		
-		String command = args[0];
-		
-		if ("-i".equals (command)) {
-			// TODO ask for URL
-			System.out.println("Enter an URL: ");
-		} else if (!command.startsWith("http")) {
+			if (args.length==0) {
 			System.out.println(getInstructions());
 			return;
 			
 		}
 		
-		System.out.println("Select what to do next: ");
-		System.out.println("1) Save page");
-		System.out.println("2) Display title");
-		System.out.println("3) Find a word");
-		System.out.println("4) List all url's on the page");
+			String command = args[0];
+			String url;
+			
+			if ("-u".equals (command)) {
+				// TODO ask for URL
+				System.out.println("Enter an URL: ");
+			} else if (!command.startsWith("https://") ) {
+				System.out.println(getInstructions());
+				return;
+				
+			}
+			ArrayList <String> file = new ArrayList <String>();
+			
+			try {
+			 file = getOpenFile (command);
+			} catch (Exception e){};
+			
+			Scanner in = new Scanner (System.in);
+			System.out.println("Select what to do next: ");
+			
+		for (int i=0; i<commands.length; i++) {
+			System.out.println(commands [i]);
+		}
+		System.out.println();
 		
+		byte choice = in.nextByte();
 		
+		switch (choice) {
 		
-		
-	}
+		case 1:
+			
+		    PrintWriter outputStream = null;
+	
+			    try {
+			        outputStream = new PrintWriter(new FileWriter("HTMLpage.txt"));
+			        outputStream.print (file); 
+			        }
 
+		 catch(Exception ex){
+			    	System.out.println("Smth. wrong" + ex);
+			    } finally {
+			    	if (outputStream!=null){
+			    		outputStream.close();
+			    	}
+			    }
+
+			    System.out.println("You saved your page. Your file is: HTMLpage.txt");
+			
+			break;
+			
+		case 2:
+			
+			
+			
+			System.out.println("Title of your page is");
+			
+			break;
+			
+		case 3:
+			
+			System.out.println("Input a word you find");
+			
+			break;
+			
+		case 4:
+			
+			System.out.println("all url's on the page");
+			
+			break;
+		
+		}
+	
+		
+
+	}
 	
 	public static String getInstructions () {
 		return "Usage: some web address\n" + "           or -i\n" + "Example: http//google.com\n";
 	}
 	
+	public static ArrayList<String> getOpenFile (String url) {
+		
+			URL url1;
+		    InputStream is = null;
+		    BufferedReader br;
+		    String line;
+		    ArrayList <String> fileString = new ArrayList <String>();
+		    
+		    try {
+		        url1 = new URL(url);
+		        is = url1.openStream();
+		        br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		        
+		        while ((line = br.readLine()) != null) {
+		        	fileString.add(line);
+		        }
+		    }catch(Exception ex){
+		    	System.out.println("Smth. wrong" + ex);
+		    } finally {
+		    	if (is != null){
+		    		try {
+						is.close();
+						} catch (IOException e) {}
+		    	}
+		    }
+		
+		return fileString;
+		
+	}
 	
 	
 }
